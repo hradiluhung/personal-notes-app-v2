f7f7257df8b94d2021435dbe3215295fe3af42aa
Hakam Royhan Adiluhung <hakamra107@gmail.com>
2022-10-01 12:08:49 +0700

    'test'

--- src/components/LoginInput.js
+++ src/components/LoginInput.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { FiMail, FiKey } from "react-icons/fi";
 import LocaleContext from "../contexts/LocaleContext";
